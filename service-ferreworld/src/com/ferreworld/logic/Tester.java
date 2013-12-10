366ea4ae85a0a2c54b876db3723d51b69647a2c9
Kelwin <kelgwiin@gmail.com>
2013-12-10 00:18:51 -0430

    Added Tester.java

---
+++ service-ferreworld/src/com/ferreworld/logic/Tester.java
@@ -0,0 +1,13 @@
+package com.ferreworld.logic;
+
+public class Tester {
+	
+	public static void main(String[] args){
+		ProductoServer ps = new  ProductoServer();
+		
+		ps.anularCategoria(5);
+		
+		
+	}
+
+}
